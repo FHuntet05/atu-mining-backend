55fb4a564211285dd66409e742ecee823ae119d4
Feft23 <129078356+FHuntet05@users.noreply.github.com>
2025-06-27 00:44:59 -0400

    rrrr

--- config/tasks.js
+++ config/tasks.js
@@ -1,8 +1,8 @@
 // En: atu-mining-backend/config/tasks.js
 const TASKS = [
-    { id: 1, title: 'Únete a nuestro grupo', reward: 500, type: 'join_group', link: 'https://t.me/+HxX28_Pvwqo3YTYx', icon: 'FiUsers' },
-    { id: 2, title: 'Compra tu primer Boost', reward: 1000, type: 'first_boost', icon: 'FiZap' },
-    { id: 3, title: 'Invita a 10 Amigos activos', reward: 1000, type: 'invite_10', icon: 'FiGift' },
+    { id: 1, title: 'Únete a nuestro grupo', reward: 1000, type: 'join_group', link: 'https://t.me/+HxX28_Pvwqo3YTYx', icon: 'FiUsers' },
+    { id: 2, title: 'Compra tu primer Boost', reward: 1500, type: 'first_boost', icon: 'FiZap' },
+    { id: 3, title: 'Invita a 10 Amigos activos', reward: 1500, type: 'invite_10', icon: 'FiGift' },
     // Puedes añadir más tareas aquí. El 'icon' es el nombre del icono de react-icons/fi
 ];
 
