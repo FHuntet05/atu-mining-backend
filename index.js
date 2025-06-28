ce56a0d2cfdfed7534fc071bf6bf83ef4f45f388
Feft23 <129078356+FHuntet05@users.noreply.github.com>
2025-06-28 05:00:16 -0400

    refactor: add Markup import from telegraf

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const { Telegraf } = require('telegraf');
+const { Telegraf , Markup } = require('telegraf');
 
 // --- 1. IMPORTAMOS RUTAS Y SERVICIOS ---
 const userController = require('./controllers/userController');
